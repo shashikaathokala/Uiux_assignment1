2f3fd32b0b3e8cfe8cfae7e1928d08110b5e805e
shashikaathokala <shashikaa.thokala@gmail.com>
2022-02-23 01:41:08 +0530

    importing functions from other file

--- 1_stack.js
+++ 1_stack.js
@@ -1,30 +1,4 @@
-// import { push,pop,peek } from "./stackfunctions.js";
-
-// class  Stack
-// {
-//     last;
-//     arr;
-//     tol_size;
-
-//     constructor(tol_size)
-//     {
-//         this.last = -1;
-//         this.arr=[];
-//         this.tol_size = tol_size;
-//     }
-//     push()
-//     pop()
-//     peek()
-    
-// }
-
-
-// let stack1 = new Stack(2);
-
-// stack1.push(1);
-// stack1.push(2);
-// stack1.pop();
-// stack1.peek();
+import { push,pop,peek } from "./stackfunctions.js";
 
 class  Stack
 {
@@ -38,44 +12,10 @@ class  Stack
         this.arr=[];
         this.tol_size = tol_size;
     }
-    push(item)
-    {
-        if(this.last==this.tol_size-1)
-        {
-            console.log("stack is full (stack overflow )");
-        }
-        else
-        {
-            this.last += 1;
-            this.arr[this.last] =item;
-            console.log(`item pushed is ${item}`)
-            return this.last;
-        }
-    }
+    push()
     pop()
-    {
-        if(this.last==-1)
-        {
-            console.log("stack is empty(underflow)");
-        }
-        else
-        {
-            console.log(`popped element is ${this.arr[this.last]}`);
-            this.last-=1;
-            return this.last;
-        }
-    }
     peek()
-    {
-        if(this.last==-1)
-        {
-            console.log("empty stack ");
-        }
-        else
-        {
-            console.log(`the topmost element is ${this.arr[this.last]}`);
-        }
-    }
+    
 }
 
 
@@ -83,7 +23,67 @@ let stack1 = new Stack(2);
 
 stack1.push(1);
 stack1.push(2);
-stack1.push(6);
 stack1.pop();
-
 stack1.peek();
+
+// class  Stack
+// {
+//     last;
+//     arr;
+//     tol_size;
+
+//     constructor(tol_size)
+//     {
+//         this.last = -1;
+//         this.arr=[];
+//         this.tol_size = tol_size;
+//     }
+//     push(item)
+//     {
+//         if(this.last==this.tol_size-1)
+//         {
+//             console.log("stack is full (stack overflow )");
+//         }
+//         else
+//         {
+//             this.last += 1;
+//             this.arr[this.last] =item;
+//             console.log(`item pushed is ${item}`)
+//             return this.last;
+//         }
+//     }
+//     pop()
+//     {
+//         if(this.last==-1)
+//         {
+//             console.log("stack is empty(underflow)");
+//         }
+//         else
+//         {
+//             console.log(`popped element is ${this.arr[this.last]}`);
+//             this.last-=1;
+//             return this.last;
+//         }
+//     }
+//     peek()
+//     {
+//         if(this.last==-1)
+//         {
+//             console.log("empty stack ");
+//         }
+//         else
+//         {
+//             console.log(`the topmost element is ${this.arr[this.last]}`);
+//         }
+//     }
+// }
+
+
+// let stack1 = new Stack(2);
+
+// stack1.push(1);
+// stack1.push(2);
+// stack1.push(6);
+// stack1.pop();
+
+// stack1.peek();
